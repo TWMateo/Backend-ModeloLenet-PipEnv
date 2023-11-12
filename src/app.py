6e8598e572e12b88d522a7923c52410da53f6735
TWMateo <fmchancosic@utn.edu.ec>
2023-11-11 23:25:08 -0500

    Refactorización de app.py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import os
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request
 from .cnn import predict_digit
+from keras.models import load_model
+import cv2
+import numpy as np
 
 
 app = Flask(__name__)
@@ -19,11 +22,31 @@ def predict_image():
 
     filepath = save_image(image)
 
-    prediction = predict_digit(filepath)
+    # prediction = predict_digit(filepath)
 
-    delete_image(filepath)
+    # delete_image(filepath)
 
-    return prediction
+    # Load the model
+    model_path = os.path.join(os.path.dirname(__file__), 'cnn','cnn_model.h5')
+    model = load_model(model_path)
+
+    # Read the image
+    img = cv2.imread(filepath)
+    gray = np.dot(img[..., :3], [0.299, 0.587, 0.114])
+
+    # reshape the image
+    gray = gray.reshape(1, 28, 28, 1)
+    # normalize image
+    gray /= 255
+
+    prediction = model.predict(gray.reshape(1, 28, 28, 1))
+
+    return {
+        "message": "success",
+        "predicted_number": float(prediction.argmax())
+    }
+
+    #return prediction
 
 
 def save_image(image) -> str:
