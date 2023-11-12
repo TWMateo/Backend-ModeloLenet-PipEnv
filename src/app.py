4349cc4706ddb76003dc641a0c0fe21f0b9b183a
TWMateo <fmchancosic@utn.edu.ec>
2023-11-12 02:34:09 -0500

    Refactorización de metodos del proyecto

--- src/app.py
+++ src/app.py
@@ -5,16 +5,16 @@ from .cnn import predict_digit
 from keras.models import load_model
 import cv2
 import numpy as np
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app,origins="*")
+#cors = CORS(app,origins="*")
 app.config['files'] = os.path.join(os.path.dirname(__file__), 'files')
 
 
 @app.get("/")
 def show_homepage():
-    return render_template("upload_image.html")
+    return 'Modelo Lenet - Backend'
 
 
 @app.post("/predict")
@@ -23,21 +23,13 @@ def predict_image():
 
     filepath = save_image(image)
 
-    # prediction = predict_digit(filepath)
-
-    # delete_image(filepath)
-
-    # Load the model
     model_path = os.path.join(os.path.dirname(__file__), 'cnn','cnn_model.h5')
     model = load_model(model_path)
 
-    # Read the image
     img = cv2.imread(filepath)
     gray = np.dot(img[..., :3], [0.299, 0.587, 0.114])
 
-    # reshape the image
     gray = gray.reshape(1, 28, 28, 1)
-    # normalize image
     gray /= 255
 
     prediction = model.predict(gray.reshape(1, 28, 28, 1))
@@ -47,7 +39,6 @@ def predict_image():
         "prediccion": float(prediction.argmax())
     }
 
-    #return prediction
 
 
 def save_image(image) -> str:
